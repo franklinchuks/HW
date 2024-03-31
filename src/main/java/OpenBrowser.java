3c99d089f7108354d12cff987d259cc410f295ef
franklin <franklinchukwuemeka234@gmail.com>
2024-03-31 23:56:01 +0300

    input fields

--- src/main/java/OpenBrowser.java
+++ src/main/java/OpenBrowser.java
@@ -1,5 +1,6 @@
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
@@ -9,10 +10,22 @@ public class OpenBrowser {
 
     @Test(priority=1)
     public void openBrowser() {
-        System.setProperty("web-driver.chrome.driver", "chromedriver");
+        System.setProperty("web-driver.chrome.driver", "../chromedriver");
         driver = new ChromeDriver();
         driver.get("https://www.swedbank.lt/business/finance/trade/factoring?language=ENG");
         String actualTitle = driver.getTitle();
         System.out.println("Title Of Website: " + actualTitle);
+    }
+
+    @Test(dependsOnMethods = {"openBrowser"})
+    public void browserInputs() throws InterruptedException {
+        WebElement invoiceAmountField = driver.findElement(By.name("calc_d5"));
+        invoiceAmountField.sendKeys("700");
+
+        Thread.sleep(5000);
+    }
+    @AfterTest
+    public void closeBrowser() {
+        driver.quit();
     }
 }
