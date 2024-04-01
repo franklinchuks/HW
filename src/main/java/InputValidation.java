121a6300bed85307be398783e9a0db5c9d181edc
franklin <franklinchukwuemeka234@gmail.com>
2024-04-01 04:03:40 +0300

    input validation tests

--- src/main/java/InputValidation.java
+++ src/main/java/InputValidation.java
@@ -4,48 +4,63 @@ import org.testng.annotations.*;
 
 public class InputValidation {
     private WebDriver driver;
+    private WebElement invoiceAmountField;
+    private WebElement interestRateField;
+    private WebElement commissionFeeField;
+    private Select advanceRateSelect;
+    private Select paymentTermSelect;
 
     @BeforeClass
     public void setUp() {
         driver = Utils.getWebDriver();
-    }
-
-    @Test
-    public void inputValidation() throws InterruptedException {
         driver.get("https://www.swedbank.lt/business/finance/trade/factoring?language=ENG");
         WebElement cookieAcceptButton = driver.findElement(By.xpath("//button[contains(@class, 'button ui-cookie-consent__accept-button')]"));
         cookieAcceptButton.click();
 
-        WebElement invoiceAmountField = driver.findElement(By.name("calc_d5"));
-        WebElement interestRateField = driver.findElement(By.name("calc_d7"));
-        WebElement commissionFeeField = driver.findElement(By.name("calc_d9"));
+        invoiceAmountField = driver.findElement(By.name("calc_d5"));
+        interestRateField = driver.findElement(By.name("calc_d7"));
+        commissionFeeField = driver.findElement(By.name("calc_d9"));
         WebElement advanceRateDropdown = driver.findElement(By.name("calc_d6"));
         WebElement paymentTermDropdown = driver.findElement(By.name("calc_d8"));
 
         invoiceAmountField.clear();
         interestRateField.clear();
         commissionFeeField.clear();
+        advanceRateSelect = new Select(advanceRateDropdown);
+        paymentTermSelect = new Select(paymentTermDropdown);
+    }
 
+    @Test
+    public void firstInputValidationTest() throws InterruptedException {
         invoiceAmountField.sendKeys("7");
         interestRateField.sendKeys("4");
         commissionFeeField.sendKeys("0.5");
-
-        Select advanceRateSelect = new Select(advanceRateDropdown);
         advanceRateSelect.selectByValue("85");
-
-        Select paymentTermSelect = new Select(paymentTermDropdown);
         paymentTermSelect.selectByValue("120");
+    }
 
-        Thread.sleep(3000);
-
-//        click calculate button
-        driver.findElement(By.xpath("//button[contains(@class, 'button -guiding')]")).click();
-
-        Thread.sleep(5000);
+    @Test
+    public void secondInputValidationTest() throws InterruptedException {
+        invoiceAmountField.sendKeys("5");
+        interestRateField.sendKeys("2");
+        commissionFeeField.sendKeys("0.5");
+        advanceRateSelect.selectByValue("90");
+        paymentTermSelect.selectByValue("60");
     }
 
     @AfterClass
     public void tearDown() {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        driver.findElement(By.xpath("//button[contains(@class, 'button -guiding')]")).click();
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         driver.quit();
     }
 }
